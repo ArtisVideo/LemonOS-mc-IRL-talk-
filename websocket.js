b907e3abcb4736331d81464a993e9695cdb56600
Artis Video <59803898+ArtisVideo@users.noreply.github.com>
2021-01-30 20:44:56 +0000

    removed PHP

--- websocket.js
+++ websocket.js
@@ -1,25 +1,21 @@
-//Web scocket (WIP Wating for access to web server)
-
-
-
 //Create WebSocket
-//var ws = new WebSocket("ws://127.0.0.1");
+var ws = new WebSocket("ws://127.0.0.1");
 
 //When it works print
-//ws.addEventListener('open', function (event) {
-//    socket.send('It be working');
-//    console.log('It be working')
-//});
+ws.addEventListener('open', function (event) {
+    socket.send('It be working');
+    console.log('It be working')
+});
 
 //Print out message when incoming
-//ws.addEventListener('message', function (event) {
-//    console.log('Message - ', event.data);
-//});
+ws.addEventListener('message', function (event) {
+    console.log('Message - ', event.data);
+});
 
 //Test function
-//function yeet() {
-//    window.close();
-//} 
+function yeet() {
+    window.close();
+} 
 
 //Safe close
 function bye() {
