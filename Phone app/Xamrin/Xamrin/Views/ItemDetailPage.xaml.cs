2d7a6efaa3132c4a9c17f4dcfc4d074c1c1b8e36
ArtisVideo <cyberskely@gmail.com>
2021-01-31 18:06:33 +0000

    added xamrin

---
+++ Phone app/Xamrin/Xamrin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamrin.ViewModels;
+
+namespace Xamrin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
