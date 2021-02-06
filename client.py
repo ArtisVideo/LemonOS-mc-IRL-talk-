b04eabd509d23d8f0efca51aa2f65fb2d293196b
Artis Video <59803898+ArtisVideo@users.noreply.github.com>
2021-02-06 21:23:51 +0000

    removed redundant code

--- client.py
+++
@@ -1,9 +0,0 @@
-while True:
-    from websocket import create_connection
-    ws = create_connection("ws://Oscar-PC:25565")
-    #Text = input("Please gib me some text - ")
-    Text = input("")
-    ws.send(Text)
-    #print("Sent")
-    #result =  ws.recv()
-    #sprint("Received '%s'" % result)
--- revieve.py
+++
@@ -1,5 +0,0 @@
-while True:
-    from websocket import create_connection
-    ws = create_connection("ws://192.168.0.101:25565")
-    message =  ws.recv()
-    print(message)
